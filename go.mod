ce21a999fa981f33138c7c6eef9bba4e701ffa44
Hasty Granbery <hasty@currant.com>
2019-08-26 15:40:20 -0700

    Convert to go modules

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/currantlabs/gorazor
+
+go 1.12
+
+require (
+	github.com/hoisie/web v0.1.0
+	golang.org/x/sys v0.0.0-20180514143608-7c87d13f8e83
+	gopkg.in/fsnotify.v1 v1.4.7
+)
--- gorazor/test/inline_var.go
+++ gorazor/test/inline_var.go
@@ -3,13 +3,17 @@ package cases
 import (
 	"bytes"
 	"github.com/currantlabs/gorazor/gorazor"
-	"github.com/sunfmin/gorazortests/models"
 )
+
+type Author struct {
+	Name string
+	Age int
+}
 
 func Inline_var() string {
 	var _buffer bytes.Buffer
 	_buffer.WriteString("\n\n<body>")
-	_buffer.WriteString(gorazor.HTMLEscape(Hello("Felix Sun", "h1", 30, &models.Author{"Van", 20}, 10)))
+	_buffer.WriteString(gorazor.HTMLEscape(Hello("Felix Sun", "h1", 30, &Author{"Van", 20}, 10)))
 	_buffer.WriteString("\n</body>")
 
 	return _buffer.String()
